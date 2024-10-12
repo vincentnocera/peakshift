7bc2a07705df38a67847f103e4b327bebf41698f
vincentnocera <eyelets.ochre_0x@icloud.com>
2024-10-11 17:34:01 -0700

    made back to home button on under construction page

--- app/under-construction/page.tsx
+++ app/under-construction/page.tsx
@@ -1,5 +1,13 @@
 'use client'
+import Link from 'next/link'
 
 export default function UnderConstruction() {
-  return <div className="flex h-screen items-center justify-center">Under construction, check back soon...  In the meantime, check out the case simulator!</div>
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="mb-4">Under construction, check back soon...  In the meantime, check out the case simulator!</div>
+      <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-green-600 transition-colors">
+        Back to Home
+      </Link>
+    </div>
+  )
 }
